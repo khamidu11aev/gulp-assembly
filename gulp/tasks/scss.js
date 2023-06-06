a5571c8719eaa7fa4fff88121640f28c22e70fc9
Sirojiddin <khamidu11ayevsergo@gmail.com>
2023-06-06 20:23:23 +0500

    js files

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -2,6 +2,12 @@ import dartSass from 'sass';
 import gulpSass from 'gulp-sass';
 import rename from 'gulp-rename';
 
+import cleanCss from 'gulp-clean-css'; // Сжатие CSS файла
+import webpcss from 'gulp-webpcss'; // Вывод WEBP изображений
+import autoprefixer from 'gulp-autoprefixer'; // Добавление вендорных префиксов
+import groupCssMediaQueries from 'gulp-group-css-media-queries'; // Групировка медиа запросов
+
+
 const sass = gulpSass(dartSass);
 
 export const scss = () => {
@@ -13,6 +19,21 @@ export const scss = () => {
         .pipe(sass({
             outputStyle: 'expanded'
         }))
+        .pipe(groupCssMediaQueries())
+        .pipe(webpcss(
+            {
+                webpClass: '.webp',
+                noWebpClass: '.no-webp'
+            }
+        ))
+        .pipe(autoprefixer({
+            grid: true,
+            overrideBrowserlist: ["last 3 versions"],
+            cascade: true
+        }))
+         // Рассмотреть не сжатый файл
+        .pipe(app.gulp.dest(app.path.build.css))
+        .pipe(cleanCss())
         .pipe(rename({
             extname: ".min.css"
         }))
--- gulpfile.js
+++ gulpfile.js
@@ -20,6 +20,7 @@ import { reset } from "./gulp/tasks/reset.js";
 import { html } from "./gulp/tasks/html.js";
 import { server } from "./gulp/tasks/server.js";
 import { scss } from "./gulp/tasks/scss.js";
+import { js } from "./gulp/tasks/main.js";
 
 
 // Наблюдатель за изменениями в файлах
@@ -27,10 +28,11 @@ function watcher() {
     gulp.watch(path.watch.files, copy);
     gulp.watch(path.watch.html, html);
     gulp.watch(path.watch.scss, scss);
+    gulp.watch(path.watch.js, js);
 }
 
 // Основные задачи
-const mainTasks = gulp.parallel(copy, html, scss)
+const mainTasks = gulp.parallel(copy, html, scss, js)
 
 // Просмотр сценариев выполнения задач
 const dev = gulp.series(reset, mainTasks, copy, gulp.parallel(watcher, server));
