47c67a8db036a872677a9a7d3e61261bca57979f
Sirojiddin <khamidu11ayevsergo@gmail.com>
2023-06-07 16:04:47 +0500

    fonts

--- gulpfile.js
+++ gulpfile.js
@@ -21,6 +21,8 @@ import { html } from "./gulp/tasks/html.js";
 import { server } from "./gulp/tasks/server.js";
 import { scss } from "./gulp/tasks/scss.js";
 import { js } from "./gulp/tasks/main.js";
+import { images } from "./gulp/tasks/images.js";
+import { otfToTtf, ttfToWoff, fontsStyle } from "./gulp/tasks/fonts.js";
 
 
 // Наблюдатель за изменениями в файлах
@@ -29,10 +31,14 @@ function watcher() {
     gulp.watch(path.watch.html, html);
     gulp.watch(path.watch.scss, scss);
     gulp.watch(path.watch.js, js);
+    gulp.watch(path.watch.images, images);
 }
 
+// Последовательная обработка шрифтов
+const fonts = gulp.series(otfToTtf, ttfToWoff, fontsStyle);
+
 // Основные задачи
-const mainTasks = gulp.parallel(copy, html, scss, js)
+const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, js, images));
 
 // Просмотр сценариев выполнения задач
 const dev = gulp.series(reset, mainTasks, copy, gulp.parallel(watcher, server));
